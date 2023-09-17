186104d2162a612fe0327efa43bfe91273d908ae
Rahul6158 <128017087+Rahul6158@users.noreply.github.com>
2023-09-17 17:00:03 +0530

    Update itotxt.py

--- itotxt.py
+++ itotxt.py
@@ -180,30 +180,6 @@ def main():
             # Convert the translated text to a Word document
             word_output_file = "translated_text.docx"
             convert_text_to_word_doc(translated_text, word_output_file)
-
-            # Display the Word document as HTML
-            with open(word_output_file, "rb") as f:
-                doc_content = convert_word_doc_to_html(f)
-                html_data = f"""
-            <!DOCTYPE html>
-            <html>
-            <head>
-            <style>
-            /* Define a CSS class for text with a specific color */
-            .custom-text {{
-                color: white; /* Change 'white' to the desired color */
-            }}
-            </style>
-            </head>
-            <body>
-            <!-- Apply the CSS class to the Word document content -->
-            <p class="custom-text">{doc_content}</p>
-            </body>
-            </html>
-            """
-                st.subheader("Preview of Translated Text as Word Document:")
-                st.components.v1.html(html_data, width=600, height=800)
-            
             # Provide a download link for the Word document
                 st.markdown(get_binary_file_downloader_html("Download Word Document", word_output_file, 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'), unsafe_allow_html=True)
 
